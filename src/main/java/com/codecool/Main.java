1dbbc846c6d408feaeffb9cb9d181c3db72721cf
TudorDan <tudor.ist@gmail.com>
2020-07-18 19:32:00 +0300

    Add tests for file read and read lines

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -1,4 +1,14 @@
 package com.codecool;
 
+import java.io.IOException;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+        FilePartReader filePartReader = new FilePartReader();
+        String path = "src/main/resources/test";
+
+        filePartReader.setup(path, 1, 1);
+        System.out.println(filePartReader.readLines());
+    }
 }
--- src/test/java/com/codecool/FilePartReaderTest.java
+++ src/test/java/com/codecool/FilePartReaderTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilePartReaderTest {
@@ -37,5 +39,42 @@ class FilePartReaderTest {
     @Test
     public void testFromLineIsSmallerThan1() {
         assertThrows(IllegalArgumentException.class, () -> filePartReader.setup(path, 0, 1));
+    }
+
+    @Test
+    public void testReadReturnsFileContent() throws IOException {
+        filePartReader.setup(path, 1, 1);
+        assertNotNull(filePartReader.read());
+    }
+
+    @Test
+    public void testFileNotInFilePathThrowsException() {
+        String wrongPath = "src/main/resources/noFile";
+        filePartReader.setup(wrongPath, 1, 1);
+        assertThrows(IOException.class, () -> filePartReader.read());
+    }
+
+    @Test
+    public void testReadLinesReadsFileWithRead() throws IOException {
+        filePartReader.setup(path, 1, 1);
+        assertNotNull(filePartReader.readLines());
+    }
+
+    @Test
+    public void testReadReturnsCorrectValue() throws IOException {
+        String correctValue = "With death being the only certainty of life, life is just a bit too short for regrets" +
+                "." + "\n" + "In all chaotic beauty lies a wounded work of art." + "\n" + "Beautiful but torn, " +
+                "wreaking havoc on my heart." + "\n" + "Camouflaged by insecurities, blinded by it all." + "\n" +
+                "Anna, Rotator, Racecar, Kayak." + "\n" + "Red rum, sir, is murder.";
+        filePartReader.setup(path, 1, 1);
+        assertEquals(correctValue, filePartReader.read());
+    }
+
+    @Test
+    public void testReadLinesReturnsCorrectValue() throws IOException {
+        String correctValue = "Beautiful but torn, wreaking havoc on my heart." + "\n" + "Camouflaged by " +
+                "insecurities, blinded by it all.";
+        filePartReader.setup(path, 3, 4);
+        assertEquals(correctValue, filePartReader.readLines());
     }
 }
